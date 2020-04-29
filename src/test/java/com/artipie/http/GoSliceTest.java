8918702b39da2d5c8acc59dfbb772217d46c3e05
olenagerasimova <olena.gerasimova@gmail.com>
2020-04-29 08:56:16 +0300

    #42 CR

--- src/test/java/com/artipie/http/GoSliceTest.java
+++ src/test/java/com/artipie/http/GoSliceTest.java
@@ -51,20 +51,13 @@ class GoSliceTest {
 
     @Test
     void returnsInfo() throws ExecutionException, InterruptedException {
-        final Storage storage = new InMemoryStorage();
         final String path = "news.info/some/day/@v/v0.1.info";
         final String body = "{\"Version\":\"0.1\",\"Time\":\"2020-01-24T00:54:14Z\"}";
-        storage.save(
-            new KeyFromPath(path),
-            new Content.From(body.getBytes())
-        ).get();
         MatcherAssert.assertThat(
-            new GoSlice(storage).response(
+            new GoSlice(GoSliceTest.storage(path, body)).response(
                 GoSliceTest.line(path), Headers.EMPTY, Flowable.empty()
             ),
-            new AllOf<>(
-                matchers(body, "application/json")
-            )
+            matchers(body, "application/json")
         );
     }
 
@@ -73,12 +66,10 @@ class GoSliceTest {
         final String path = "example.com/mod/one/@v/v1.mod";
         final String body = "bla-bla";
         MatcherAssert.assertThat(
-            new GoSlice(GoSliceTest.get(path, body)).response(
+            new GoSlice(GoSliceTest.storage(path, body)).response(
                 GoSliceTest.line(path), Headers.EMPTY, Flowable.empty()
             ),
-            new AllOf<>(
-                matchers(body, "text/plain")
-            )
+            matchers(body, "text/plain")
         );
     }
 
@@ -87,12 +78,10 @@ class GoSliceTest {
         final String path = "modules.zip/foo/bar/@v/v1.0.9.zip";
         final String body = "smth";
         MatcherAssert.assertThat(
-            new GoSlice(GoSliceTest.get(path, body)).response(
+            new GoSlice(GoSliceTest.storage(path, body)).response(
                 GoSliceTest.line(path), Headers.EMPTY, Flowable.empty()
             ),
-            new AllOf<>(
-                matchers(body, "application/zip")
-            )
+            matchers(body, "application/zip")
         );
     }
 
@@ -101,12 +90,10 @@ class GoSliceTest {
         final String path = "example.com/list/bar/@v/list";
         final String body = "v1.2.3";
         MatcherAssert.assertThat(
-            new GoSlice(GoSliceTest.get(path, body)).response(
+            new GoSlice(GoSliceTest.storage(path, body)).response(
                 GoSliceTest.line(path), Headers.EMPTY, Flowable.empty()
             ),
-            new AllOf<>(
-                matchers(body, "text/plain")
-            )
+            matchers(body, "text/plain")
         );
     }
 
@@ -115,10 +102,21 @@ class GoSliceTest {
         final String path = "example.com/abc/def";
         final String body = "v1.8.3";
         MatcherAssert.assertThat(
-            new GoSlice(GoSliceTest.get(path, body)).response(
+            new GoSlice(GoSliceTest.storage(path, body)).response(
                 GoSliceTest.line(path), Headers.EMPTY, Flowable.empty()
             ),
             new RsHasStatus(RsStatus.NOT_FOUND)
+        );
+    }
+
+    @Test
+    void returnsLatest() throws ExecutionException, InterruptedException {
+        final String body = "{\"Version\":\"1.1\",\"Time\":\"2020-01-24T00:54:14Z\"}";
+        MatcherAssert.assertThat(
+            new GoSlice(GoSliceTest.storage("example.com/latest/bar/@v/v1.1.info", body)).response(
+                GoSliceTest.line("example.com/latest/bar/@latest"), Headers.EMPTY, Flowable.empty()
+            ),
+            matchers(body, "application/json")
         );
     }
 
@@ -128,11 +126,13 @@ class GoSliceTest {
      * @param type Content-type
      * @return List of matchers
      */
-    private static ListOf<Matcher<? super Response>> matchers(final String body,
+    private static AllOf<Response> matchers(final String body,
         final String type) {
-        return new ListOf<Matcher<? super Response>>(
-            new RsHasBody(body.getBytes()),
-            new RsHasHeaders(new MapEntry<>("content-type", type))
+        return new AllOf<>(
+            new ListOf<Matcher<? super Response>>(
+                new RsHasBody(body.getBytes()),
+                new RsHasHeaders(new MapEntry<>("content-type", type))
+            )
         );
     }
 
@@ -153,7 +153,7 @@ class GoSliceTest {
      * @throws ExecutionException On error
      * @throws InterruptedException On error
      */
-    private static Storage get(final String path, final String body)
+    private static Storage storage(final String path, final String body)
         throws ExecutionException, InterruptedException {
         final Storage storage = new InMemoryStorage();
         storage.save(
